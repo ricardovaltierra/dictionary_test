8b317577d7faccb954a50f0f512a9d27fc8171fc
Oscar <orivas155@gmail.com>
2022-06-08 11:13:37 -0500

    add lib files

---
+++ lib/dictionary.rb
@@ -0,0 +1,1 @@
+# Escribe tu código del ejercicio en este archivo. Haz que las pruebas en test/dictionary_test.rb pasen
---
+++ lib/word.rb
@@ -0,0 +1,1 @@
+# Escribe tu código del ejercicio en este archivo. Haz que las pruebas en test/word_test.rb pasen
