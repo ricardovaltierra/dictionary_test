867fed97a45b4794c87b7a8ed58bedfa68351236
ricardovaltierra <ricardo_valtierra@outlook.com>
2022-07-22 12:45:25 -0500

    Word and Dictionary class created via TDD

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,1 +1,15 @@
 # Escribe tu código del ejercicio en este archivo. Haz que las pruebas en test/dictionary_test.rb pasen
+
+class Dictionary
+  attr_accessor :dictionary_words
+
+  def initialize(dictionary_words)
+    @dictionary_words = dictionary_words
+  end
+
+  def find_meaning(word)
+    @dictionary_words.find do |word_with_meaning|
+      word_with_meaning.word == word
+    end.meaning
+  end
+end
--- lib/word.rb
+++ lib/word.rb
@@ -1,1 +1,9 @@
 # Escribe tu código del ejercicio en este archivo. Haz que las pruebas en test/word_test.rb pasen
+class Word
+  attr_accessor :word, :meaning
+
+  def initialize(word, meaning)
+    @word = word
+    @meaning = meaning
+  end
+end
